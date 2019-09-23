22e6fe542a05b46c4450dd4ccab2a2286be1df05
PaperHS <paperhs901230@gmail.com>
2019-09-23 16:00:12 +0800

    add hash query

--- src/main/kotlin/com/example/forgejavademo/DemoApplication.kt
+++ src/main/kotlin/com/example/forgejavademo/DemoApplication.kt
@@ -34,37 +34,54 @@ fun main(args: Array<String>) {
 class InitLine : CommandLineRunner {
 	var logger = LoggerFactory.getLogger("Forge Init")
 	override fun run(vararg args: String?) {
+		// Connect forge chain node
 		val forge = ForgeSDK.connect("localhost",28210)
+		// Query Chain info
 		val chainInfo = forge.getChainInfo(RequestGetChainInfo.getDefaultInstance()).info
-		val forgeState = forge.getForgeState(RequestGetForgeState.getDefaultInstance()).state
-		//    logger.info("\nchain info:\n$chainInfo")
+		logger.info("\nchain info:\n$chainInfo\n\n\n")
 
+		// Query forge current state
+		val forgeState = forge.getForgeState(RequestGetForgeState.getDefaultInstance()).state
+
+		// create two wallet
 		val alice = forge.createWallet("alice","123qweASD")
 		val bob = forge.createWallet("bobbb","123qweASD")
+
 		Thread.sleep(5000) //wait for block to commit
 
+		// create a stream to listen account state
 		val accountRequest = forge.getAccountState(object : StreamObserver<ResponseGetAccountState> {
 			override fun onNext(value: ResponseGetAccountState?) {
-				logger.info("\nAccountState state:\n${BigInteger(value?.state?.balance?.value?.toByteArray())}")
+				logger.info("\nAccountState balance:\n${BigInteger(value?.state?.balance?.value?.toByteArray())}")
 			}
-			override fun onError(t: Throwable?) {
-			}
-			override fun onCompleted() {
-			}
+			override fun onError(t: Throwable?) {}
+			override fun onCompleted() {}
 		})
 		accountRequest.onNext(RequestGetAccountState.newBuilder().setAddress(alice.wallet.address).build())
+		val hashQuery = forge.getTx(object : StreamObserver<Rpc.ResponseGetTx>{
+			override fun onError(t: Throwable?) {} override fun onCompleted() {}
+			override fun onNext(value: Rpc.ResponseGetTx?) {
+				logger.info("\nHash State:\n${value?.info}")
+			}
+		})
 
+		// create a Poke transaction and send
 		val tx = TransactionFactory.unsignPoke(forgeState.pokeConfig.address,chainInfo.network, WalletInfo(alice.wallet)).signTx(alice.wallet.sk.toByteArray())
 		val response = forge.sendTx(tx)
 		logger.info("\npoke:\n$response")
 
 		Thread.sleep(5000) //wait for block to commit
+		//Query account balance and transaction hash
 		accountRequest.onNext(RequestGetAccountState.newBuilder().setAddress(alice.wallet.address).build())
-		Thread.sleep(5000)
+		hashQuery.onNext(Rpc.RequestGetTx.newBuilder().setHash(response.hash).build())
+
+
+		//create a transfer tx and send
 		forge.transfer(alice.wallet, bob.wallet, BigDecimal("2E18").toBigInteger())
-		
+
 		Thread.sleep(5000) //wait for block to commit
+		//Query account balance again
 		accountRequest.onNext(RequestGetAccountState.newBuilder().setAddress(alice.wallet.address).build())
-		Thread.sleep(5000)
+
 	}
 }
