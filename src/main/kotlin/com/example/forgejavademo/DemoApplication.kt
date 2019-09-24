e8cc05b12a0406309d77aafb0f15df48fe630d13
PaperHS <paperhs901230@gmail.com>
2019-09-24 17:24:54 +0800

    add asset

--- src/main/kotlin/com/example/forgejavademo/DemoApplication.kt
+++ src/main/kotlin/com/example/forgejavademo/DemoApplication.kt
@@ -1,5 +1,6 @@
 package com.example.forgejavademo
 
+import forge_abi.Enum
 import forge_abi.Enum.HashType.sha3
 import forge_abi.Enum.KeyType.ed25519
 import forge_abi.Enum.RoleType.role_account
@@ -9,11 +10,8 @@ import forge_abi.Rpc.RequestGetChainInfo
 import forge_abi.Rpc.RequestGetForgeState
 import forge_abi.Rpc.ResponseGetAccountState
 import forge_abi.Type
-import io.arcblock.forge.ForgeSDK
-import io.arcblock.forge.TransactionFactory
+import io.arcblock.forge.*
 import io.arcblock.forge.did.WalletInfo
-import io.arcblock.forge.sendTx
-import io.arcblock.forge.signTx
 import io.grpc.stub.StreamObserver
 import org.slf4j.LoggerFactory
 import org.springframework.boot.CommandLineRunner
@@ -47,7 +45,7 @@ class InitLine : CommandLineRunner {
 		// create two wallet
 		val alice = forge.createWallet("alice","123qweASD")
 		val bob = forge.createWallet("bobbb","123qweASD")
-
+		logger.info("\n\n\n Alice\n $alice\n\n\n")
 		Thread.sleep(5000) //wait for block to commit
 
 		// create a stream to listen account state
@@ -60,9 +58,12 @@ class InitLine : CommandLineRunner {
 		})
 		accountRequest.onNext(RequestGetAccountState.newBuilder().setAddress(alice.wallet.address).build())
 		val hashQuery = forge.getTx(object : StreamObserver<Rpc.ResponseGetTx>{
-			override fun onError(t: Throwable?) {} override fun onCompleted() {}
+			override fun onError(t: Throwable?) {
+				logger.error(t?.message)
+			} override fun onCompleted() {}
 			override fun onNext(value: Rpc.ResponseGetTx?) {
-				logger.info("\nHash State:\n${value?.info}")
+				logger.info("\n\n\nHash State:\n$value")
+				logger.info("\nCode::${value?.code}")
 			}
 		})
 
@@ -70,6 +71,8 @@ class InitLine : CommandLineRunner {
 		val tx = TransactionFactory.unsignPoke(forgeState.pokeConfig.address,chainInfo.network, WalletInfo(alice.wallet)).signTx(alice.wallet.sk.toByteArray())
 		val response = forge.sendTx(tx)
 		logger.info("\npoke:\n$response")
+		hashQuery.onNext(Rpc.RequestGetTx.newBuilder().setHash(response.hash).build())
+
 
 		Thread.sleep(5000) //wait for block to commit
 		//Query account balance and transaction hash
@@ -103,6 +106,8 @@ class InitLine : CommandLineRunner {
 		queryAsset.onNext(Rpc.RequestGetAssetState.newBuilder().setAddress(assetAddress)
 			.build())
 		Thread.sleep(1000) //wait for block to commit
+
+//		forge.exchange(alice,bob, BigInteger.TEN,assetAddress)
 
 	}
 }
